6ccbe23de3e05c32e66090f0dd4968739d34be84
Sophie Gu <sophieqgu@gmail.com>
2021-01-12 04:56:30 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,7 +1,7 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: birds, only: [:id, :name, :species]
+    render json: @birds, only: [:id, :name, :species]
   end
 
   def show
